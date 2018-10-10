a8f48be21d885b67cab3e0f5bf1025fa47286157
Erika Szabo <erika.szabo.geo@gmail.com>
2018-10-10 22:12:46 +0000

    implemented form validation, checking if the tweet is empty or too long

--- public/scripts/ajax-form.js
+++ public/scripts/ajax-form.js
@@ -1,9 +1,17 @@
 $(function() {            //submits the new tweets to the database
   $(".tweet-form").on('submit', function (event) {
-    event.preventDefault();     //prevents default "submit" event
-    $.ajax('/tweets', { method: 'POST' , data: $(this).serialize()})
-    .then(function () {
-      console.log("New Tweet!!");
-    });
+    event.preventDefault();        //prevents default "submit" event
+    tweetText = $(this).children('#text-area').val();  //can use also just simple $(#text-area) instead of working with this
+    console.log("tweetText: ", tweetText);
+    if (tweetText === '' || tweetText === null ) {
+      alert('Write something!!!');
+    } else {
+      if (tweetText.length > 140) {
+        alert('Tweet too looooong!!')
+      } else {
+          $.ajax('/tweets', { method: 'POST' , data: $(this).serialize()})
+          .then(loadTweets);
+      }
+    }
   });
 });
