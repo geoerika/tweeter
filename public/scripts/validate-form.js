b7ecb3a9d5ec45a4d806ea1155cf757201aa47d9
Erika Szabo <erika.szabo.geo@gmail.com>
2018-10-25 13:12:47 +0000

    replaced loadTweets function with a location.reload() in the ajax POST line

--- public/scripts/validate-form.js
+++ public/scripts/validate-form.js
@@ -2,10 +2,12 @@
 
 $(function() {
 
-  $('#text-area').on('click',   //current errorMessage hidden once clik occurs in the textarea
-    function (event) {
-      $('#errorMessage').slideToggle();
-  });
+  // $('#text-area').on('click',   //current errorMessage hidden once clik occurs in the textarea
+  //   function (event) {
+  //     event.preventDefault();
+  //     $('#errorMessage').slideToggle();
+  //     $('#errorMessage').empty(); //clear error message
+  // });
 
   $(".tweet-form").on('submit', function (event) {      //submits the new tweet to the database
 
@@ -14,21 +16,31 @@ $(function() {
     tweetText = $('#text-area').val();    //gets the value of tweet text
     $('#errorMessage').empty();         //we clear the errorMessage
 
-    if (tweetText === '' || tweetText === null ) {
+    if (tweetText === '' || tweetText === null ) {  //handles errors for empty tweets
         $('#errorMessage').html('Error! Empty Message!');
-        $('#errorMessage').slideToggle();
+        $('#errorMessage').slideToggle(); //toggle of error message
 
     } else {
 
-        if (tweetText.length > 140) {
+        if (tweetText.length > 140) { //handles errors for too long tweets
           $('#errorMessage').html('Tweet too long!');
-          $('#errorMessage').slideToggle();
+          $('#errorMessage').slideToggle(); //toggle of error message
 
         } else {
           $.ajax('/tweets', { method: 'POST' , data: $(this).serialize()})    //records the tweet in the datatbase
-            .then(loadTweets).then(() => {$('#text-area').val(''); $('#counter').text('140')});  //loads tweets and resets new tweet
-                                                                                               // textarea and the character counter
+            .then(() => {
+              $('#text-area').val('');    //resets new tweet textarea and the character counter
+              $('#counter').text('140');
+              location.reload();      //refreshes the tweet pages to display the new tweet in the list of tweets
+            });
         }
       }
+  });
+
+  $('#text-area').on('click',   //current errorMessage hidden once clik occurs in the textarea
+    function (event) {
+      event.preventDefault();
+      $('#errorMessage').slideToggle();
+      $('#errorMessage').empty(); //clear error message
   });
 })
