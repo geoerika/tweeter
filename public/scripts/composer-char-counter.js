99bc21516a72faa723de58d6011352e8221e6705
Erika Szabo <erika.szabo.geo@gmail.com>
2018-10-25 13:13:52 +0000

    added comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,12 +5,12 @@ $(document).ready(function() {
   $('#text-area' ).keyup(function(event) {
 
     let countChar = $(this);
-    countChar.closest('.new-tweet').find('#counter').text(140 - this.value.length);
+    countChar.closest('.new-tweet').find('#counter').text(140 - this.value.length); //looks for the character count in the new-tweet class
     if (this.value.length > 140) {
-      $('#counter').addClass('redColour');
+      $('#counter').addClass('redColour'); //colours character count in red if tweet text > 140 characters
     }
     else {
-      $('#counter').removeClass('redColour');
+      $('#counter').removeClass('redColour');  //removes ed colour from character count
     }
   });
 });
