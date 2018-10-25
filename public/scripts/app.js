e4eebdc85b43274bd9223d2c3f786b6f46fd2951
Erika Szabo <erika.szabo.geo@gmail.com>
2018-10-25 13:11:09 +0000

    deleted return in the renderTweets and loadTweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,3 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 
 const daysAgoCalc = (created_at) => {   //calculates days since tweet was sent
 
@@ -14,56 +9,55 @@ const createTweetElement = (tweet) => {             //creates an html tweet elem
 
   let $tweet = $("<article>").addClass("tweet");
 
-  let avatar = $(`<img src="${tweet.user.avatars.small}"></img>`);
-  let h2Header = $(`<h2 class='userName' >${tweet.user.name}</h2>`);
-  let spanHeader = $(`<span>${tweet.user.handle}</span>`);
+  let avatar = $(`<img src="${tweet.user.avatars.small}"></img>`);   //selects avatar image
+  let h2Header = $(`<h2 class='userName' >${tweet.user.name}</h2>`);  //selects user name
+  let spanHeader = $(`<span>${tweet.user.handle}</span>`);  //selects user handle
 
-  let header = $("<header>").append(avatar, h2Header, spanHeader);
+  let header = $("<header>").append(avatar, h2Header, spanHeader); //creates the header
 
-  let tweetContent = tweet.content.text;
+  let tweetContent = tweet.content.text;        //retrieves tweet text
   let tweetDiv = $("<p>").text(tweetContent);   //fixes Cross-Site Scripting using text()
 
-  let divTweet = $("<div>").append(tweetDiv);
+  let divTweet = $("<div>").append(tweetDiv);  //adds elements to div for displaying tweet text
 
-  let timeFooter = $(`<span id='time'>${daysAgoCalc(tweet.created_at)} days ago</span>`);
+  let timeFooter = $(`<span id='time'>${daysAgoCalc(tweet.created_at)} days ago</span>`);  //retrieves time since tweet was created
+  // adds icons to the tweet footer
   let iconFooter = $("<span class='icons'><ion-icon name='flag'></ion-icon> <ion-icon name='repeat'></ion-icon> <ion-icon name='heart'></ion-icon></span>");
 
-  let divFooter = $("<div>").append(timeFooter, iconFooter);
+  let divFooter = $("<div>").append(timeFooter, iconFooter); //creates the footer
 
-  let footer = $("<footer>").append(divFooter);
+  let footer = $("<footer>").append(divFooter); //adds elements to footer
 
   return $tweet.append(header, divTweet, footer);
 };
 
 
-const renderTweets = (data) => {      //loads the tweets in the html tweets-container and adds the last tweet as newest
+const renderTweets = (data) => {      // creates tweets from a list of tweets
 
   data.forEach(tweetData => {
-    let $tweet = createTweetElement(tweetData);
-    $('#tweets-container').prepend($tweet);
+    let $tweet = createTweetElement(tweetData);  //creates tweet element for each tweet in the list retrieved from the database
+    $('#tweets-container').prepend($tweet); //the last tweet added in the database will be shown first in the tweet list
   });
-
-  return $('#tweets-container');
 };
 
 
-const loadTweets = () => {    //gets all the tweets from the database
+const loadTweets = () => {    //loads all the tweets from the database into a list and displays them on the page
 
   $.ajax('/tweets', {method: 'GET'})
     .then(function(tweets) {
-      return renderTweets(tweets);
+      renderTweets(tweets);
   })
 };
 
 
 $(document).ready(function() {
 
-  $('#compose').click(    //checks if the compose button was clicked and executes slide
+  $('#compose').click(    //checks if the compose button was clicked and executes slide for new tweet container
       function() {
         $('.new-tweet').slideToggle();
-        $('#text-area').focus();
+        $('#text-area').focus(); //focuses cursor in the text area
       }
   )
 
-  loadTweets();
+  loadTweets(); //loads tweets from the database on the page
 });
