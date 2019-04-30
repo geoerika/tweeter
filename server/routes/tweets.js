23e65823716ca19b7986729a5b1dc359f0ac8589
Erika Szabo <erika.szabo.geo@gmail.com>
2019-04-29 23:32:34 -0400

    included updateLikes

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -23,23 +23,36 @@ module.exports = function(DataHelpers) {
       return;
     }
 
-    const user = req.body.user ? req.body.user : userHelper.generateRandomUser();
-    const tweet = {
-      user: user,
-      content: {
-        text: req.body.text
-      },
-      created_at: Date.now(),
-      likes: 0
-    };
+    if (req.body.user) {
 
-    DataHelpers.saveTweet(tweet, (err) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-      } else {
-        res.status(201).send();
-      }
-    });
+      DataHelpers.updateLikes(req.body //updates likes in existing tweets
+        , (err) => {
+        if (err) {
+          res.status(500).json({ error: err.message });
+        } else {
+          res.status(201).send();
+        }
+      });
+
+    } else {
+
+      tweet = { //creates new tweets
+        user: userHelper.generateRandomUser(),
+        content: {
+          text: req.body.text
+        },
+        created_at: Date.now(),
+        likes: 0
+      };
+
+      DataHelpers.saveTweet(tweet, (err) => {    //saves tweets
+        if (err) {
+          res.status(500).json({ error: err.message });
+        } else {
+          res.status(201).send();
+        }
+      });
+    }
   });
 
   return tweetsRoutes;
