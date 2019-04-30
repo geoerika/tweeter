259993d587a69b83be8bf54f0e142018a74b3105
Erika Szabo <erika.szabo.geo@gmail.com>
2019-04-29 23:27:29 -0400

    implemented jQuery function to uodate likes for a tweet

---
+++ public/scripts/likes.js
@@ -0,0 +1,26 @@
+"use strict";
+
+$(document).ready(function() {
+
+  $(".tweet").on("click", ".tweet", function (event) {
+
+    event.preventDefault();  //prevents default "click" event;
+
+    let tweet = $(this).closest('.tweet');  //finds the tweet that got a like
+    let user = tweet.find(".userName").text();
+    let text = tweet.find("p").text();
+    let likes = tweet.find('#likes').text();
+
+    if (tweet.find('.heart').css('color') === 'rgb(255, 0, 0)') {
+      likes--;
+      tweet.find('.heart').css({'color':'#00a087'});
+    } else {
+      likes++;
+      tweet.find('.heart').css({'color':'red'});
+    }
+    tweet.find('#likes').text(likes);
+    likes = tweet.find('#likes').text();
+
+    $.ajax('/tweets', { method: 'POST', data: {user:user, text:text, likes:likes}});   //updates the tweet likes in the datatbase
+  })
+});
