2e68d473e948919a2f2b9fdcc7e53a52fe5c2d37
Erika Szabo <erika.szabo.geo@gmail.com>
2019-05-02 19:05:18 -0400

    deleted commented code

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -2,21 +2,8 @@
 
 const userHelper    = require("../lib/util/user-helper")
 
-// const bodyParser = require('body-parser')
 const express       = require('express');
-// const methodOverride = require('method-override')
 const tweetsRoutes  = express.Router();
-
-// tweetRoutes.use(bodyParser.urlencoded());
-
-// tweetRoutes.use(methodOverride(function (req, res) {
-//   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-//     // look in urlencoded POST bodies and delete it
-//     var method = req.body._method
-//     delete req.body._method
-//     return method
-//   }
-// }));
 
 module.exports = function(DataHelpers) {
 
